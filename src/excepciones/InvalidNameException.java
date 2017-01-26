d06d6ba4dccb1efa2ab6b7dae462404285a59fd7
adepel00 <adepel00@estudiantes.unileon.es>
2017-01-26 19:12:07 +0100

    Delete InvalidNameException.java

--- src/excepciones/InvalidNameException.java
+++
@@ -1,8 +0,0 @@
-package excepciones;
-
-public class InvalidNameException extends Exception{
-	@Override
-	public String getMessage(){
-		return "Su nombre y apellidos sólo pueden contener letras";
-	}
-}
