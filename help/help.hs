2568003e06ae1d72c3497f5314c2f7d0e58022d2
adepel00 <adepel00@estudiantes.unileon.es>
2017-01-28 00:22:18 +0100

    Add files via upload

---
+++ help/help.hs
@@ -0,0 +1,30 @@
+<<?xml version="1.0" encoding='ISO-8859-1' ?>
+<!DOCTYPE helpset
+PUBLIC "-//Sun Microsystems Inc.//DTD JavaHelp HelpSet Version 1.0//EN"
+"http://java.sun.com/products/javahelp/helpset_1_0.dtd">
+<helpset version="1.0">
+   <title>Ejemplo ayuda JavaHelp</title>
+   <maps>
+      <!-- Pagina por defecto al mostrar la ayuda -->
+      <homeID>ventana_inicio</homeID>
+      <!-- Que mapa deseamos -->
+      <mapref location="mapa.jhm"/>
+   </maps> 
+
+   <!-- Las Vistas que deseamos mostrar en la ayuda -->
+   <!-- La tabla de contenidos --> 
+   <view>
+      <name>Tabla Contenidos</name>
+      <label>Tabla de contenidos</label>
+      <type>javax.help.TOCView</type>
+      <data>tablacontenidos.xml</data>
+   </view> 
+
+   <!-- El indice --> 
+   <view>
+      <name>Indice</name>
+      <label>El indice</label>
+      <type>javax.help.IndexView</type>
+      <data>indice.xml</data>
+   </view>
+</helpset>
