3bd2aff571b3c1cae9485d74ff95df0631ec8ff1
EvaHergar <eherng03@estudiantes.unileon.es>
2017-01-27 13:48:19 +0100

    AlreadyExistException añadida

---
+++ src/excepciones/AlreadyExistException.java
@@ -0,0 +1,10 @@
+package excepciones;
+
+public class AlreadyExistException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public String getMessage(){
+		return "Los datos introducidos ya exiten.";
+	}
+}
